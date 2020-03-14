5d81c49ef0c7ba709884c2cf3d9685f963ff2843
Sek1Nek0 <l1beral@l1beral.ink>
2020-03-14 21:26:33 +0800

    2020-03-14
    1.词库优化
    2.UI 优化
    3.支持生成 入团申请书

---
+++ generator/CYLApplication.js
@@ -0,0 +1,96 @@
+let CYLApplicationFisrtParagraphSentence = [
+    '一直以来我都认为加入共青团是一件光荣的事，因为共青团是中国领导的先进青年的组织，是的助手和后备军。',
+    '一直以来我都对已经加入了中国共青团的同学非常羡慕。',
+    '我志愿加入中国共青团，请团支部在考虑我的情况后，做出决定。',
+    '我通过团章的学习，认识到作为一个21世纪的青年，必须积极争取加入先进青年的群众组织——中国共产主义青年团。',
+    '今天我怀着激动的心情郑重的递交我的入团申请书：我志愿加入中国共产主义青年团。',
+    '我志愿加入中国共青团，成为中国共青团的一员，我将把我的青春奉献给美好的共青团，对于我的能力，希望组织给予肯定。',
+
+];
+let CYLApplicationFirstParagraphSentences = [
+    '团是党的助手，是无产阶级的先锋队组织，她时刻代表着我们年轻的一代奋发向上、敢于斗争、永于拼搏、自强不息。',
+    '中国共产主义青年团是青年中的先锋队，是接受共产主义教育的学校，是中国共产党忠实的后备力量。',
+    '在中国共产主义青年团的帮助教育下，我们能够成为热爱祖国、忠于人民，有知识、守纪律，体魄健壮，勤劳勇敢，朝气蓬勃，不怕任何困难的共产主义接班人。',
+    '通过学习团章，我对中国共产主义青年团有了初步的认识与了解。共青团是大家的骄傲与向往，“是中国共产党的助手和后备军”。我们应以共青团为荣，更要为共青团献一份力。',
+    '多年来，共青团象磁石一样吸引着我，我爱党、爱祖国，我深知共青团是党的助手，建设祖国的英勇突击队，我渴望加入这个组织并为此而努力。',
+    '中国共产主义青年团是党领导下的先进青年的群众组织，是党的可靠，得力的后备军。他能带领我们参加有益身心的事情，从而使我们在实际中慢慢成长。',
+    '在成为一名团员后，我要在团、在党的领导下，树立正确的思想观念，这是一切行动的前提。',
+
+];
+
+let CYLApplicationFirstMainParagraphSentence = [
+    '随着阅历的丰富，我对团组织有了初步的认识：共青团是中国共产党的有力助手，是一座培养人才、锻炼人才的大熔炉。',
+    '我时刻以团员的标准来严格要求自己，在学习上，我以团员为学习榜样，以超越他们为目标。',
+    '如果我光荣地加入共青团，我会认真地履行团员的义务，做到宣传、执行党的基本路线，努力完成团组织交给的任务，在学习、劳动、工作及其他社会活动中起模范作用。',
+    '我志愿加入中国共产主义青年团，因为我知道中国共产主义青年团是中国共产党领导的先进青年群众组织。',
+    '梦，不能说珍贵，也不能说平庸。一个简简单单的字眼，却能衍生出许多道理来。每个人都有权力拥有它。',
+
+];
+let CYLApplicationMainParagraphSentences = [
+    '在生活上，我注意勤俭节约，我讨厌向父母伸手讨钱，所以我想努力学习各种知识，希望能早日独立自强，自力更生。我决心加入中国共青主义青年团，决心追求我的愿望，希望能够为社会作出更多贡献。',
+    '我一定会遵守团章，执行团的决议，遵守团的纪律，履行团员的义务，参加团的工作，作为一名名副其实的共青团员，处处起到模范作用，为“四化”贡献自己的全部力量，为祖国的未来继续努力。',
+    '入团后，我将更严格要求自己，坚决拥护中国共产党的领导，遵守团的章程，执行团的决议，履行团员义务，严守团的纪律，把团的名誉放在首位。',
+    '我一定会勤奋学习，积极工作，关心集体和国家大事，爱护他人，力争做一名优秀的共青团员，为建设我们伟大的社会主义祖国贡献自己的力量。',
+    '我无论在校内、校外都踊跃参加有利于团名誉的事，为团争光，为团做贡献，做一名名符其实的共青团员，处处起模范作用。',
+    '在加入共青团后，在自我素质的提高之外，我会尽自己所能去改善这个组织，积极参加实践活动。',
+    '如果我能够加入到共青团这个优秀的组织当中，我一定会加倍努力，让自己的能力更加全面，同时严格遵守团的章程和团对于团员的相关规定。',
+    '我能够发挥团员的先进性作用，让同学们都来了解共青团的发展历史和时代责任，扩大团的影响力。',
+];
+
+let CYLApplicationEndingParagraph = [
+    '如果团组织批准我的入团申请，使我成为一名光荣的共青团员，我将按照团的章程，认真履行团员义务， 按时交纳团费，坚决执行团组织的决议，充分发挥共青团员的先锋模范作用，做品学兼优的好学生。如果团组织暂时没有批准我的申请，我也不灰心丧气，将继续努 力，积极向团组织靠拢，向好的同学看齐，努力克服自己身上存在的不足，争取早日加入团组织。',
+    '遵守团的决定，承认团的章程，履行团的章程，履行团的义务，执行团的纪律，服从团的决定，保守团的秘密，按时交纳团费是做为一名青年的准则。我决心从各方面严格要求自己，工作踏实肯干，积极要求上进，做一名名副其实的共青团员。',
+    '如果团组织能批准我入团,我决心遵守团的纪律、团的章程，执行团的决议，吃苦在前，享受在后，做一个名副其实的共青团员，继续努力，爬上最高峰。如果我未被批准，我也会按照一个团员的标准去严格要求自己，决不放弃，补足自己的缺点，接受更多的考验，争取早日入团。',
+    '如果团组织批准我的入团申请，使我成为一名共青团员，我将按照团的章程，认真履行团员义务，坚决执行团组织的决议，充分发挥共青团员的先锋模范作用。如果团支部认为我还不够标准，我会继续努力，争取下次的入团机会。',
+    '如果团组织暂时没有批准我的申请，我也不灰心丧气，将继续努力，积极向团组织靠拢，向好的同学看齐，努力克服自己身上存在的不足，争取早日加入团组织。',
+
+];
+
+/**
+ * @return {string}
+ */
+function CYLApplicationGenerateFirstParagraph() {
+    let usedRandomNumber = [randomNumber(CYLApplicationFirstParagraphSentences.length)][0];
+    //console.log(usedRandomNumber);
+    //console.log(firstParagraph);
+    return CYLApplicationFisrtParagraphSentence[randomNumber(CYLApplicationFisrtParagraphSentence.length)[0]] + CYLApplicationFirstParagraphSentences[usedRandomNumber[0]] + CYLApplicationFirstParagraphSentences[usedRandomNumber[1]];
+}
+
+/**
+ * @return {string}
+ */
+function CYLApplicationGenerateMainParagraph() {
+    let usedRandomNumber = [randomNumber(CYLApplicationMainParagraphSentences.length)][0];
+    //console.log(usedRandomNumber);
+    //console.log(main);
+    return CYLApplicationFirstMainParagraphSentence[randomNumber(CYLApplicationFirstMainParagraphSentence.length)[0]] + CYLApplicationMainParagraphSentences[usedRandomNumber[0]] + CYLApplicationMainParagraphSentences[usedRandomNumber[1]] + CYLApplicationMainParagraphSentences[usedRandomNumber[2]] + CYLApplicationMainParagraphSentences[usedRandomNumber[3]];
+}
+
+function CYLApplicationGenerateCYLApplicationEndingParagraph() {
+    let usedRandomNumber = [randomNumber(CYLApplicationEndingParagraph.length)][0];
+    //console.log(usedRandomNumber);
+    //console.log(ending);
+    return CYLApplicationEndingParagraph[usedRandomNumber[0]];
+}
+
+/**
+ * @return {string}
+ */
+function CYLApplicationGenerate() {
+    return '敬爱的团支部：' + '\n\n' + CYLApplicationGenerateFirstParagraph() + '\n' + CYLApplicationGenerateMainParagraph() + '\n' + CYLApplicationGenerateCYLApplicationEndingParagraph()  + '\n\n' + '此致'  + '\n' + '敬礼！' + '\n' + '申请人：XXX' + '\n' + 'XXXX年X月X日';
+}
+
+function CYLApplicationGenerated() {
+    document.getElementById("generatedContent").value = CYLApplicationGenerate();
+}
+
+function randomNumber(num) {
+    const ary = [];					//创建一个空数组用来保存随机数组
+    for (let i = 0; i < num; i++) {			//按照正常排序填充数组
+        ary[i] = i;
+    }
+    ary.sort(function () {
+        return 0.5 - Math.random();		//返回随机正负值
+    });
+    return ary;					//返回数组
+}
---
+++ generator/pneumoniaArticle.js
@@ -0,0 +1,105 @@
+let pneumoniaArticleFirstParagraphSentence = [
+    '今年迎来了新型冠状病毒，中国人民又面临着一次巨大的危机，我希望它尽快被消灭。',
+    '今年迎来了新型冠状病毒，我以为武汉离我们这么远，这病毒也过不来，因此我们照常生活，上学，一切都风平浪静。',
+    '2020年即将来临之际，一场名为“2020新冠肺炎”的疫病，从武汉一个不起眼的海鲜市场之中，疯狂的肆虐开来。',
+    '由武汉为传染源引发的新型冠状病毒向全国扩散，中华人民距离上次2003年的非典仅过去17年，刚好降临在春节之际。',
+    '自12月份武汉爆发疫情以来，今年的春节就释放出了朦胧的压迫感。我们处在十几线城市，完全料想不到这个春节不出门，不上街，不串门，多日“宅”在家中的情景。',
+    '在进入2020的第一个月就遭到当头一棒——武汉肺炎。一时间，往日热闹温馨的春节街头变得清清冷冷，行人寥寥。',
+    '疫情是命令十万火急，防控是责任刻不容缓。当前，全国各地疫情防控形势严峻。',
+];
+let pneumoniaArticleFirstParagraphSentences = [
+    '2003年的"非典"想必大家都知道了，一场恐怖的瘟疫，为此许多鲜活的生命离开了这个美好的世界。',
+    '对抗新型关于2020新冠肺炎，已不仅是政府部门或者医务工做作者的事情，它与我们每个人都休戚相关。',
+    '一时间，往日热闹温馨的春节街头变得清清冷冷，行人寥寥；火红的商家店铺纷纷闭市歇业；千千万万的游子身处异地，不能回家团聚。',
+    '新冠肺炎贪婪地将一个个无辜者抓住，妄想将他们拖入死亡的深渊，妄想让这个春天弥漫着腐败的恶气。',
+    '“新型肺炎”牵动着世界的目光，这注定是一个不一样的春节。',
+];
+
+let pneumoniaArticleFirstMainParagraphSentence = [
+    '它开始肆虐起来。',
+    '春节期间，我们在刷抖音时，看到很多医生和护士，为了不让家人担心，以出差为借口，奋不顾身地奔向抗击病毒的最前线。',
+    '武汉出现了几例病毒感染者，我并没有太在意，还以为武汉离我们这么远，这病毒也过不来。',
+    '这段时日以来，疫情多，谣言多，发灾难财者有，然而赤子之心扶国者众。',
+    '这段时日以来，防护物资少，街上冷清，与亲友见面也少。',
+    '朋友们，还记得2003年的非典吗?那是我记忆中的第一场没有硝烟的战争。当时的我们曾无知过。',
+];
+let pneumoniaArticleMainParagraphSentences = [
+    '很多国家也伸出援手向中国捐献了口罩和生化服，是巴基斯坦向中国捐献了三十万个口罩，八百套生化服，感谢他们，为了人民，前仆后继，奋勇逆行！' +
+    '许多照片上，医护人员卸下防护口罩后，脸上被勒出了深深的痕迹。',
+    '春节期间，我们在刷抖音时，看到很多医生和护士，为了不让家人担心，以出差为借口，奋不顾身地奔向抗击病毒的最前线。',
+    '有一位年逾八旬的老人，2003年非典爆发之时，他奔赴疫区，主持我国“非典”等传染病诊治指南，为战胜“非典”疫情作出重要贡献，然而，在武汉新型关于2020新冠肺炎的作文毒抗击中，他再次临危受命，出任国家卫健委高级别专家组组长，他就是84岁的钟南山教授。',
+    '有网友所说：“春晚，是我们中国人的精神图腾，也是凝聚中国力量的所在，无论在哪，我们的心都在一起。',
+    '中华民族从5000多年的艰难困苦中走来，逐渐形成了百折不挠、坚韧不拔的民族品格。新中国70年一路走来，从来不是一帆风顺。',
+    '截止1月28日，全国已有130支医疗队到达武汉。各路救援人员奔赴武汉防疫战场：当年抗击非典的郭军主任，84岁高龄的钟南山院士。',
+    '在抗击新型冠状病毒感染的肺炎疫情中，人们纷纷向医护人员、党员干部、媒体记者等一线英雄致敬。是他们坚守岗位、默默奉献，构成了确保人民生命安全和身体健康的“钢铁长城”，撒播着令世人钦佩的人间大爱。',
+    '文学源自生活，高于生活。春晚节目情景报告《爱是桥梁》，生动刻画和倾情讴歌了疫情防控阻击战现场白衣天使、先锋模范、无冕之王们的“战斗姿态”。',
+    '“隔离病毒，没有隔离爱”成为了人们的一致共识，在网上坊间传递了无限正能量，也坚定了全国人民敢于斗争、赢取胜利的信心和决心。',
+    '秩序是现代文明社会的标志。面对日益加重的疫情防控工作，人们看到，一些地方先后采取了“封城”“封村”“封园”等必要的应急措施。',
+    '不少地区纷纷组织封城，封村，这无疑带来了物价飞涨，一颗白菜上标价68，是何等的黑心商家，口罩纷纷涨价，由几块涨到几时块，又让多少人望口罩兴叹。然而，这当中并不缺乏好人，来自世界各地的仁人志士纷纷进行捐款。',
+    '奋斗在一线的白衣天使更是倾尽全力，他们放弃了与家人团聚的机会，放弃了安逸的生活，毅然决然奔赴“战场”，年过古稀的钟南山教授依然在与病毒抗争，每位医护人员都值得我们尊敬。',
+    '17年后的今天，我们再次遇到了同非典一般的疫情，我们怕什么？',
+];
+
+let pneumoniaArticleFirstEndingParagraphSentence = [
+    '只要我们紧紧团结在一起，就没有克服不了的困难。',
+    '这次疫情，举国上下，众志成城，武汉加油！',
+    '让我们每个人的力量凝聚起来，共同度过这场没有硝烟的战争。',
+    '哪怕一次次遭遇自然灾害、曲折磨难，我们从没有低过头，在所有危机关头，我们一次更比一次地众志成城，我们一回更比一回地顽强不倒。',
+    '这个冬天，突如其来的新型冠状病毒，是大海上的一片浪花，虽然给我们带来了担心、害怕，也让我们看到了属于自己的巨大智慧、勇气和决心。',
+    '抗击“新型肺炎”，使中华民族又一次彰显出伟大的民族精神。',
+    '我们众志成城，一定能够迈过新冠状病毒这道坎！祝愿我们祖国越来越强大！',
+
+
+];
+let pneumoniaArticleEndingParagraphSentences = [
+    '笼罩在中华大地上的阴霾一定会尽快散开，春风吹遍大地，人们重回美好生活。',
+    '我们每个人也要做好自己健康的第一责任。我们应养成戴口罩，勤洗手，不随地吐痰等良好习惯，这不仅是我们对自己的健康负责，更是对抗疫一线工作者的支持。',
+    '希望我们人人都能理智、勇敢、科学地防控“疫情”，让新型冠状病毒无处遁形，从此消声匿迹！',
+    '危难时刻，在民族精神的感召下，亿万人民筑起了一座抗击“新型肺炎”的钢铁长城！',
+    '无论是滔滔无边的洪水，还是鬼魅无形的病毒，在坚韧不拔的民族面前只能望而却步。',
+    '感谢为生命守岁的人们，你们承载着光明与希望，守护着平安与健康！',
+    '终于到了在家坐着也能为国家做贡献的时候了，大家应该在家中也好好学习，牢记时代赋予我们的使命，有朝一日也能站在第一线战场上成为国家的栋梁。',
+    '武汉也不是一座孤岛，它一定会好起来的，戴口罩的日子一定会很快结束！',
+    '等到春暖花开，这座城市痊愈时，我会回去湖北，去看我的爷爷奶奶！',
+
+];
+
+function pneumoniaArticleGenerateFirstParagraph() {
+    let usedRandomNumber = [randomNumber(pneumoniaArticleFirstParagraphSentences.length)][0];
+    //console.log(usedRandomNumber);
+    //console.log(firstParagraph);
+    return pneumoniaArticleFirstParagraphSentence[randomNumber(pneumoniaArticleFirstParagraphSentence.length)[0]] + pneumoniaArticleFirstParagraphSentences[usedRandomNumber[0]] + pneumoniaArticleFirstParagraphSentences[usedRandomNumber[1]];
+}
+
+function pneumoniaArticleGenerateMain() {
+    let usedRandomNumber = [randomNumber(pneumoniaArticleMainParagraphSentences.length)][0];
+    //console.log(usedRandomNumber);
+    //console.log(main);
+    return pneumoniaArticleFirstMainParagraphSentence[randomNumber(pneumoniaArticleFirstMainParagraphSentence.length)[0]] + pneumoniaArticleMainParagraphSentences[usedRandomNumber[0]] + pneumoniaArticleMainParagraphSentences[usedRandomNumber[1]] + pneumoniaArticleMainParagraphSentences[usedRandomNumber[2]] + pneumoniaArticleMainParagraphSentences[usedRandomNumber[3]];
+}
+
+function pneumoniaArticleGenerateEnding() {
+    let usedRandomNumber = [randomNumber(pneumoniaArticleEndingParagraphSentences.length)][0];
+    //console.log(usedRandomNumber);
+    //console.log(ending);
+    return pneumoniaArticleFirstEndingParagraphSentence[randomNumber(pneumoniaArticleFirstEndingParagraphSentence.length)[0]] + pneumoniaArticleEndingParagraphSentences[usedRandomNumber[0]] + pneumoniaArticleEndingParagraphSentences[usedRandomNumber[1]];
+}
+
+function pneumoniaArticleGenerate() {
+    return pneumoniaArticleGenerateFirstParagraph() + '\n' + pneumoniaArticleGenerateMain() + '\n' + pneumoniaArticleGenerateEnding();
+}
+
+function pneumoniaArticleGenerated() {
+    document.getElementById("generatedContent").value = pneumoniaArticleGenerate();
+}
+
+function randomNumber(num) {
+    const ary = [];					//创建一个空数组用来保存随机数组
+    for (let i = 0; i < num; i++) {			//按照正常排序填充数组
+        ary[i] = i;
+    }
+    ary.sort(function () {
+        return 0.5 - Math.random();		//返回随机正负值
+    });
+    return ary;					//返回数组
+}
